bf38cf5fb34faf7e73ff7ab97503c73b7fe87401
Neema-Charity <ncharityyy@gmail.com>
2024-03-19 16:27:43 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Neema Charity';
+const welcomeMessage = 'Welcome to Flatbook, Neema Charity!' 
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, NEEMA CHARITY!'
+const shortGreeting = 'Welcome, N!'
+
